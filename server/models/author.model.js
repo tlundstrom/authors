b1174cb2302818ef513886cec1e66c94ec69de69
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-03-17 12:30:02 -0700

    added min length to author name

--- server/models/author.model.js
+++ server/models/author.model.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const AuthorSchema = new mongoose.Schema({
-    name: {type: String}
+    name: {type: String,
+    required: [
+        true,
+        'Name is Required'
+    ],
+    minLength:[3, "Must be at least 3 characters long."] 
+    }
 }, {timestamps: true });
 module.exports= mongoose.model('Author', AuthorSchema);
